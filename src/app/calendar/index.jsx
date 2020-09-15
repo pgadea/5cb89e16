a7ec867ac30697e6a996846b4e186ece52fe970a
Pierre <pgadea@users.noreply.github.com>
2020-09-15 08:31:26 -0500

    custom calendar component completed

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import './styles.css';
 
-export default function Calendar({ value, onChange }) {
-  return <div>Calendar here</div>;
+import buildCalendar from './build';
+import dayStyles, {beforeToday} from './styles';
+import Header from "./header";
+
+export default function Calendar({value, onChange}) {
+  const [calendar, setCalendar] = useState([]);
+
+  useEffect(() => {
+    setCalendar(buildCalendar(value));
+  }, [value]);
+
+  return (
+    <div className='calendar'>
+     <Header value={value} setValue={onChange} />
+      <div className='body'>
+        <div className="day-names">
+          {["s", "m", "t", "w", "t", "f", "s"].map((d) => (
+            <div className="week">{d}</div>
+          ))}
+        </div>
+        {calendar.map((week) => (
+          <div>
+            {week.map((day) => (
+              <div className='day' onClick={() => !beforeToday(day) && onChange(day)}>
+                <div className={dayStyles(day, value)}>
+                  {day.format('D').toString()}
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
+import moment from 'moment';
 import Calendar from "./calendar";
 import "./styles.css";
 
-export default function () {
-  return <Calendar />;
+export default function App() {
+  const [value, setValue] = useState(moment());
+  return <Calendar value={value} onChange={setValue} />;
 }
